ee3cc04ea3b4a8c57bd2626cecf03963ff57f24a
markv <vhmark@gmail.com>
2020-04-03 21:17:54 -0700

    Fix special handling of course_module_completion_updated

--- classes/observer.php
+++ classes/observer.php
@@ -74,13 +74,15 @@ class block_integrityadvocate_observer {
                 $debug && block_integrityadvocate_log(__FILE__ . '::' . __FUNCTION__ . "::$event->eventname::Event name={$event->eventname}, so just close the IA session");
                 self::close_activity_user_session($event);
                 $useriaresults = block_integrityadvocate_get_course_user_ia_data($event->courseid, $event->userid, $event->contextid);
-                if ($useriaresults) {
-                    // If we get back a string we got an error, so skip it.
+                if ($useriaresults && $event->contextlevel === CONTEXT_MODULE) {
                     if (is_string($useriaresults)) {
+                        // If we get back a string we got an error, so skip it.
                         $debug && block_integrityadvocate_log(__FILE__ . '::' . __FUNCTION__ . "::$event->eventname::Skipped closing: " . print_r($useriaresults, true));
-                    } elseif (is_array($participant = $useriaresults[0])) {
-                        require_once('task/process_integrityadvocate.php');
-                        block_integrityadvocate\task\process_integrityadvocate::process_single_user($event->oourseid, $participant, __FUNCTION__);
+                    } elseif (is_array($participants = $useriaresults['ia_participant_data'])) {
+                        $modulecontext = $event->get_context();
+                        list($blockinstanceid, $blockinstance) = block_integrityadvocate_get_ia_block($modulecontext, $visibleonly);
+                        $debugblockidentifier = 'courseid=' . $courseid . '; moduleid=' . $modulecontext->instanceid . '; blockinstanceid=' . $blockinstanceid;
+                        block_integrityadvocate_cron_single_user($event->courseid, $modulecontext, $blockinstance, $participants[0], $debugblockidentifier);
                         return;
                     }
                 }
@@ -237,7 +239,6 @@ class block_integrityadvocate_observer {
         $debug = true;
 
         $modulecontext = $event->get_context();
-
         if ($modulecontext->contextlevel != CONTEXT_MODULE) {
             $msg = 'The passed-in event is not from a module context level';
             block_integrityadvocate_log(__FILE__ . '::' . __FUNCTION__ . "::$msg");
