ddf6d47b60aa9fe44d770d2dbb4dd282cc885b72
markv <vhmark@gmail.com>
2020-04-10 13:59:32 -0700

    Fix version s/b 202004100

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
  */
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 202041000;
+$plugin->version = 202004100;
 $plugin->requires = 2017111300; // Moodle 3.4.0.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 202041000;
+$plugin->release = 202004100;
 $plugin->component = 'block_integrityadvocate';
