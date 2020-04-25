1e6ada270c6e6018a59c5a677126e19d05e6f9b2
markv <vhmark@gmail.com>
2020-04-25 20:04:27 +0000

    Bump minimum Moodle version to 3.5.0 20180517

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version = 2020042501;
-$plugin->requires = 2017111300; // Moodle 3.4.0.
+$plugin->requires = 20180517; // Moodle 3.5.0.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 2020042501;
 $plugin->component = 'block_integrityadvocate';
