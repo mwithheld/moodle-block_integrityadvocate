0711b5a71bda28592a4898113e022541998884ee
markv <vhmark@gmail.com>
2020-04-25 19:36:22 +0000

    Bump version to 2020042501

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
  */
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2020042500;
+$plugin->version = 2020042501;
 $plugin->requires = 2017111300; // Moodle 3.4.0.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 2020042500;
+$plugin->release = 2020042501;
 $plugin->component = 'block_integrityadvocate';
