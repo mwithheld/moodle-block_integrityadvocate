67e8d1b7be7d67aac63055a1b7844e024e8a3123
vhmark@gmail.com <vhmark@gmail.com>
2020-05-30 20:49:59 -0700

    Issue #14 Update privacy API use to reflect remote data use

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -14,34 +14,344 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * Privacy Subsystem implementation for block_calendar_month.
- *
- * @package    block_integrityadvocate
- * @copyright  IntegrityAdvocate.com
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
-namespace block_integrityadvocate\privacy;
-
 defined('MOODLE_INTERNAL') || die();
 
 /**
- * Privacy Subsystem for block_calendar_month implementing null_provider.
+ * Privacy Subsystem for block_integrityadvocate.
  *
  * @copyright  IntegrityAdvocate.com
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class provider implements \core_privacy\local\metadata\null_provider {
+
+namespace block_integrityadvocate;
+
+use \core_privacy\local\metadata\collection;
+use \core_privacy\local\request;
+use block_integrityadvocate\Api as ia_api;
+use block_integrityadvocate\MoodleUtility as ia_mu;
+use block_integrityadvocate\Utility as ia_u;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/blocks/integrityadvocate/lib.php');
+
+class provider implements
+\core_privacy\local\metadata\provider
+\core_userlist_provider {
+
+    const PRIVACYMETADATA_STR = 'privacy:metadata';
+    const BRNL = "<br>\n";
 
     /**
-     * Get the language string identifier with the component's language
-     * file to explain why this plugin stores no data.
+     * Get information about the user data stored by this plugin.
      *
-     * @return  string
+     * @param  collection $collection An object for storing metadata.
+     * @return collection The metadata.
      */
-    public static function get_reason() {
-        return 'privacy:metadata';
+    public static function get_metadata(collection $collection):
+    collection {
+        $privacyitems = array(
+            // Course info.
+            'cmid',
+            // Moodle user info.
+            'email',
+            'fullname',
+            'userid',
+            // Video session info.
+            'identification_card',
+            'session_end',
+            'session_start',
+            'user_video',
+            // Override info.
+            'override_date',
+            'override_fullname',
+            'override_reason',
+            'override_status',
+        );
+
+        // Combine the above keys with corresponding values into a new key-value array.
+        $privacyitemsarr = array();
+        foreach ($privacyitems as $key) {
+            $privacyitemsarr[$key] = self::PRIVACYMETADATA_STR . ':' . INTEGRITYADVOCATE_BLOCKNAME . ':' . $key;
+        }
+
+        $collection->add_external_location_link(INTEGRITYADVOCATE_BLOCKNAME, $privacyitemsarr,
+                self::PRIVACYMETADATA_STR . ':' . INTEGRITYADVOCATE_BLOCKNAME);
+
+        return $collection;
+    }
+
+    /**
+     * Get the list of users who have data within a context.
+     * This will include users who are no longer enrolled in the context if they still have remote IA participant data.
+     *
+     * @param   \userlist    $userlist   The userlist containing the list of users who have data in this context/plugin combination.
+     */
+    public static function get_users_in_context(\userlist $userlist) {
+        global $DB;
+        $debug = false;
+        $fxn = __CLASS__ . '::' . __FUNCTION__;
+        $debug && ia_mu::log($fxn . '::Started with $userlist=' . var_export($userlist, true));
+
+        if (empty($userlist->count())) {
+            return;
+        }
+
+        // Get IA participant data from the remote API.
+        $participants = \block_integrityadvocate_get_participants_for_blockcontext($userlist->get_context());
+        $debug && ia_mu::log($fxn . '::Got count($participants)=' . (is_countable($participants) ? count($participants) : 0));
+        if (ia_u::is_empty($participants)) {
+            return;
+        }
+
+
+        // Populate this list with user ids who have IA data in this context.
+        // This lets us use add_users() to minimize DB calls rather than add_user() in the below loop.
+        $userids = array();
+        foreach ($participants as $p) {
+            // Populate if is a participant.
+            if (isset($p->participantidentifier) && !empty($p->participantidentifier)) {
+                $userids[] = $p->participantidentifier;
+            }
+
+            // Populate if is an override instructor.
+            if (isset($p->overridelmsuserid) && !empty($p->overridelmsuserid)) {
+                $userids[] = $p->overridelmsuserid;
+            }
+        }
+
+        $userlist->add_users(array_unique($userids));
+    }
+
+    /**
+     * Delete multiple users within a single context.
+     *
+     * @param approved_userlist $userlist The approved context and user information to delete information for.
+     */
+    public static function delete_data_for_users(\approved_userlist $userlist) {
+        $debug = false;
+        $fxn = __CLASS__ . '::' . __FUNCTION__;
+        $debug && ia_mu::log($fxn . '::Started with $userlist=' . var_export($userlist, true));
+
+        if (empty($userlist->count())) {
+            return;
+        }
+
+        // Get IA participant data from the remote API.
+        $participants = \block_integrityadvocate_get_participants_for_blockcontext($userlist->get_context());
+        $debug && ia_mu::log($fxn . '::Got count($participants)=' . (is_countable($participants) ? count($participants) : 0));
+        if (ia_u::is_empty($participants) || ia_u::is_empty($userlist) || ia_u::is_empty($userids = $userlist->get_userids())) {
+            return;
+        }
+
+        // Prevent multiple messages for the same user by tracking the IDs we have sent to.
+        $participantmessagesent = array();
+        $overridemessagesent = array();
+
+        foreach ($participants as $p) {
+            // Check the participant is one we should delete.
+            if (isset($p->participantidentifier) && !empty($p->participantidentifier) && in_array($p->participantidentifier, $userids)) {
+                // Request participant data delete.
+                if (!in_array($p->participantidentifier, $participantmessagesent)) {
+                    self::send_delete_request('Please remove IA participant data for ' . self::BRNL . self::get_participant_info_to_send($p));
+                    $participantmessagesent[] = $p->participantidentifier;
+                }
+            }
+
+            // Check the override user is one we should delete.
+            if (isset($p->overridelmsuserid) && !empty($p->overridelmsuserid) && in_array($p->overridelmsuserid, $userids)) {
+                // Request override instructor data delete.
+                if (!in_array($p->overridelmsuserid, $overridemessagesent)) {
+                    self::send_delete_request('Please remove IA *overrider* data for ' . self::BRNL . self::get_override_info_to_send($p));
+                    $overridemessagesent[] = $p->overridelmsuserid;
+                }
+            }
+        }
+    }
+
+    /**
+     * Delete all personal data for all users in the specified context.
+     *
+     * @param context $context Context to delete data from.
+     */
+    public static function delete_data_for_all_users_in_context(\context $context) {
+        $debug = false;
+        $fxn = __CLASS__ . '::' . __FUNCTION__;
+        $debug && ia_mu::log($fxn . '::Started with $context=' . var_export($context, true));
+
+        // Get IA participant data from the remote API.
+        $participants = \block_integrityadvocate_get_participants_for_blockcontext($context);
+        $debug && ia_mu::log($fxn . '::Got count($participants)=' . (is_countable($participants) ? count($participants) : 0));
+        if (ia_u::is_empty($participants)) {
+            return;
+        }
+
+        // Prevent multiple messages for the same user by tracking the IDs we have sent to.
+        $participantmessagesent = array();
+        $overridemessagesent = array();
+
+        foreach ($participants as $p) {
+            // Check the participant is one we should delete.
+            if (isset($p->participantidentifier) && !empty($p->participantidentifier)) {
+                // Request participant data delete.
+                if (!in_array($p->participantidentifier, $participantmessagesent)) {
+                    self::send_delete_request('Please remove IA participant data for ' . self::BRNL . self::get_participant_info_to_send($p));
+                    $participantmessagesent[] = $p->participantidentifier;
+                }
+            }
+
+            // Check the override user is one we should delete.
+            if (isset($p->overridelmsuserid) && !empty($p->overridelmsuserid)) {
+                // Request override instructor data delete.
+                if (!in_array($p->overridelmsuserid, $overridemessagesent)) {
+                    self::send_delete_request('Please remove IA *overrider* data for ' . self::BRNL . self::get_override_info_to_send($p));
+                    $overridemessagesent[] = $p->overridelmsuserid;
+                }
+            }
+        }
+    }
+
+    /**
+     * Delete all user data for the specified user, in the specified contexts.
+     *
+     * @param   approved_contextlist    $contextlist    The approved contexts and user information to delete information for.
+     */
+    public static function delete_data_for_user(\approved_contextlist $contextlist) {
+        $debug = false;
+        $fxn = __CLASS__ . '::' . __FUNCTION__;
+        $debug && ia_mu::log($fxn . '::Started with $contextlist=' . var_export($contextlist, true));
+
+        if (empty($contextlist->count())) {
+            return;
+        }
+
+        $user = $contextlist->get_user();
+
+        foreach ($contextlist->get_contexts() as $context) {
+            // Get IA participant data from the remote API.
+            $participants = \block_integrityadvocate_get_participants_for_blockcontext($context);
+            $debug && ia_mu::log($fxn . '::Got count($participants)=' . (is_countable($participants) ? count($participants) : 0));
+            if (ia_u::is_empty($participants)) {
+                continue;
+            }
+        }
+
+        // Prevent multiple messages for the same user by tracking the IDs we have sent to.
+        $participantmessagesent = array();
+        $overridemessagesent = array();
+
+        // Find the one we should delete.
+        foreach ($participants as $p) {
+            // Check the participant is one we should delete.
+            if (isset($p->participantidentifier) && !empty($p->participantidentifier) && (intval($p->participantidentifier) === intval($user->id))) {
+                // Request participant data delete.
+                $useridentifier = $context->instanceid . '-' . $p->participantidentifier;
+                if (!in_array($useridentifier, $participantmessagesent)) {
+                    self::send_delete_request('Please remove IA participant data for ' . self::BRNL . self::get_participant_info_to_send($p));
+                    $participantmessagesent[] = $useridentifier;
+                }
+            }
+
+            // Check the override user is one we should delete.
+            if (isset($p->overridelmsuserid) && !empty($p->overridelmsuserid) && (intval($p->overridelmsuserid) === intval($user->id))) {
+                // Request override instructor data delete.
+                $useridentifier = $context->instanceid . '-' . $p->overridelmsuserid;
+                if (!in_array($useridentifier, $overridemessagesent)) {
+                    self::send_delete_request('Please remove IA *overrider* data for ' . self::BRNL . self::get_override_info_to_send($p));
+                    $overridemessagesent[] = $context->instanceid . '-' . $p->overridelmsuserid;
+                }
+            }
+        }
+    }
+
+    /**
+     * Gather IA participant info to send in the delete request.
+     *
+     * @param \block_integrityadvocate\Participant $participant
+     * @return string HTML Participant info to uniquely identify the entry to IntegrityAdvocate.
+     */
+    private static function get_participant_info_to_send(Participant $participant): string {
+        $usefulfields = array(
+            'courseid',
+            'created',
+            'modified',
+            'email',
+            'firstname',
+            'lastname',
+            'overridedate',
+            'participantidentifier',
+            'status',
+        );
+
+        $info = array();
+        foreach ($usefulfields as $property) {
+            $info[] = "&nbsp;&nbsp;&bull;&nbsp;{$property}={$participant->$property}";
+        }
+
+        return implode(self::BRNL, $info);
+    }
+
+    /**
+     * Gather IA override user info to send in the delete request.
+     *
+     * @param \block_integrityadvocate\Participant $participant
+     * @return string HTML Participant and override info to uniquely identify the entry to IntegrityAdvocate.
+     */
+    private static function get_override_info_to_send(Participant $participant): string {
+        $usefulfields = array(
+            'courseid',
+            'created',
+            'modified',
+            'email',
+            'firstname',
+            'lastname',
+            'overridedate',
+            'overridelmsuserfirstname',
+            'overridelmsuserid',
+            'overridelmsuserlastname',
+            'overridestatus',
+            'participantidentifier',
+            'status',
+        );
+
+        $info = array();
+        foreach ($usefulfields as $property) {
+            $info[] = "&nbsp;&nbsp;&bull;&nbsp;{$property}={$participant->$property}";
+        }
+
+        return implode(self::BRNL, $info) . self::BRNL;
+    }
+
+    /**
+     * Email the user data delete request to INTEGRITYADVOCATE_PRIVACY_EMAIL
+     *
+     * @return bool True on emailing success; else false.
+     */
+    private static function send_delete_request(string $msg): bool {
+        global $USER, $CFG, $SITE;
+
+        // Throws an exception if email is invalid.
+        $mailto = clean_param(INTEGRITYADVOCATE_PRIVACY_EMAIL, PARAM_EMAIL);
+
+        // Try a few ways to get an email from address.
+        $mailfrom = $USER->email;
+        if (empty($mailfrom) && !empty($CFG->supportemail)) {
+            $mailfrom = $CFG->supportemail;
+        }
+        if (empty($mailfrom) && !empty($siteadmin = \get_admin()) && !empty($siteadmin->email)) {
+            $mailfrom = $siteadmin->email;
+        }
+        if (empty($mailfrom)) {
+            $mailfrom = $mailto;
+        }
+
+        $subject = 'Moodle privacy API data removal request from "' . $SITE->fullname . '" ' . $CFG->wwwroot;
+        $message = $subject . self::BRNL;
+        $message .= "Admin email={$siteadmin->email}" . self::BRNL;
+        $message .= '--' . self::BRNL;
+        $message .= $msg;
+
+        return email_to_user($mailto, $mailfrom, $subject, html_to_text($message), $message);
     }
 
 }
--- lang/en/block_integrityadvocate.php
+++ lang/en/block_integrityadvocate.php
@@ -38,6 +38,7 @@ $string['created'] = 'First seen';
 $string['disabled_editingmode'] = 'Course editing mode is enabled, so video monitoring is disabled';
 $string['disabled_haseditcap'] = 'Disabled for non-students';
 $string['disabled_notenrolled'] = 'Not available - you are not enrolled in this course';
+
 $string['error_curlcloseia'] = 'Curl error closing the IA session';
 $string['error_curlnoremoteinfo'] = 'Error: Got no remote IA participant info - check the API key and app id are valid';
 $string['error_invalidappid'] = "Invalid {$string['config_appid']} - it is a code that looks a bit like this: c56a4180-65aa-42ec-a945-5fd21dec0538";
@@ -45,18 +46,22 @@ $string['error_noapikey'] = "No {$string['config_apikey']} is set";
 $string['error_noappid'] = "No {$string['config_appid']} is set";
 $string['error_notenrolled'] = "You are not enrolled in this course";
 $string['error_quiz_showblocks'] = "This quiz is configured with &quot;Show blocks during quiz attempts&quot; = No.  To fix this, edit the quiz settings > Appearance > Show more...";
+
 $string['flag_comment'] = 'Details';
 $string['flag_capture'] = 'Captured photo';
 $string['flag_errorcode'] = 'Error code';
 $string['flag_type'] = 'Flag';
 $string['flags_none'] = 'None';
+
 $string['fullname'] = 'Full course name';
+
 $string['integrityadvocate:addinstance'] = "Add new {$string['pluginname']} block";
 $string['integrityadvocate:addinstance'] = 'Add a new ' . $string['pluginname'] . ' block';
 $string['integrityadvocate:myaddinstance'] = "Add a new {$string['pluginname']} block to the desired page";
 $string['integrityadvocate:myaddinstance'] = 'Add a ' . $string['pluginname'] . ' block to My home page';
 $string['integrityadvocate:overview'] = 'View course overview of Integrity Advocate for all students';
 $string['integrityadvocate:selfview'] = 'View own ' . $string['pluginname'] . ' results';
+
 $string['last_modified'] = 'Last modified';
 $string['lastaccess'] = 'Last in course';
 $string['no_blocks'] = 'No ' . $string['pluginname'] . ' blocks are set up for your courses.';
@@ -70,13 +75,29 @@ $string['no_remote_participants'] = 'No IA-side participants found';
 $string['no_user'] = 'No matching user found';
 $string['not_all_expected_set'] = 'Not all activities with completion have an "{$a}" date set.';
 $string['now_indicator'] = 'NOW';
+
 $string['overview'] = $string['pluginname'] . ' Overview';
 $string['overview_flags'] = 'Flags';
 $string['overview_session'] = '<h4>{$a}</h4>';
 $string['overview_sessions'] = '<h3>Sessions for this user</h3>';
 $string['overview_user_status'] = 'Latest status';
 $string['overview_view_details'] = 'View details';
+
 $string['privacy:metadata'] = 'The ' . $string['pluginname'] . ' block only displays existing completion data.';
+$string['privacy:metadata:block_integrityadvocate'] = 'This plugin stores no data in Moodle.  In order to integrate with a remote service, user data needs to be exchanged with that service.  See <a href="https://integrityadvocate.com/Home/Privacy?lang=en">Integrity Advocate Privacy</a> for more information.';
+$string['privacy:metadata:block_integrityadvocate:cmid'] = 'Id number of the course module.';
+$string['privacy:metadata:block_integrityadvocate:email'] = 'Your email address.';
+$string['privacy:metadata:block_integrityadvocate:fullname'] = 'Your full name.';
+$string['privacy:metadata:block_integrityadvocate:identification_card'] = 'A picture of your government-issued ID.';
+$string['privacy:metadata:block_integrityadvocate:override_date'] = 'If applicable, the date the ' . $string['pluginname'] . ' status is overridden by an instructor.';
+$string['privacy:metadata:block_integrityadvocate:override_fullname'] = 'If applicable, the full name of the instrucor doing the override.';
+$string['privacy:metadata:block_integrityadvocate:override_reason'] = 'If applicable, the instrucor reason for doing the override.';
+$string['privacy:metadata:block_integrityadvocate:override_status'] = 'If applicable, the ' . $string['pluginname'] . ' status applied by the override.';
+$string['privacy:metadata:block_integrityadvocate:session_end'] = 'The time your proctoring session session ends.';
+$string['privacy:metadata:block_integrityadvocate:session_start'] = 'The time your proctoring session session starts.';
+$string['privacy:metadata:block_integrityadvocate:user_video'] = 'A video (with audio) recording of you completing an activity.';
+$string['privacy:metadata:block_integrityadvocate:userid'] = 'Your database user id number.';
+
 $string['process_integrityadvocate'] = "{$string['pluginname']} - Process";
 $string['progress'] = '# proctor sessions';
 $string['resubmit_link'] = 'Resubmit your ID';
@@ -85,9 +106,11 @@ $string['session_end'] = 'End';
 $string['session_status'] = 'Status';
 $string['shortname'] = 'Short course name';
 $string['showallinfo'] = 'Show all info';
+
 $string['status_in_progress'] = 'In progress';
 $string['status_invalid_id'] = 'Invalid (ID)';
 $string['status_invalid_rules'] = 'Invalid (Rules)';
 $string['status_valid'] = 'Valid';
+
 $string['submitted'] = 'Submitted';
 $string['time_expected'] = 'Expected';
