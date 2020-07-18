e152db4d172f2a3019a47abf50cb3f3f8c6ca460
vhmark@gmail.com <vhmark@gmail.com>
2020-07-18 10:22:40 -0700

    Bump version to 2020071800

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
  */
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2020071502;
+$plugin->version = 2020071800;
 $plugin->requires = 2018051700; // Moodle 3.5.0.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 2020071502;
+$plugin->release = 2020071800;
 $plugin->component = 'block_integrityadvocate';
