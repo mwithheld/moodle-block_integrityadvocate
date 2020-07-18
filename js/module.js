caf261c1d4994ef03beddb46bd889b520b888bc6
vhmark@gmail.com <vhmark@gmail.com>
2020-07-18 15:23:29 -0700

    Add some checks to prevent double-loading IA

--- js/module.js
+++ js/module.js
@@ -26,7 +26,8 @@ M.block_integrityadvocate = {
 
         var is_quiz_attempt = (document.body.id === 'page-mod-quiz-attempt');
         var is_scorm_entry = (document.body.id === 'page-mod-scorm-view');
-        window.console.log('is_scorm_entry=', is_scorm_entry);
+        var elt_scorm_enter = jQuery('#scormviewform input[type="submit"]');
+        var elt_usernotifications = jQuery('#user-notifications');
 
         //@url https://stackoverflow.com/a/31350391
         function decodeEntities(encodedString) {
@@ -35,16 +36,19 @@ M.block_integrityadvocate = {
             return textArea.value;
         }
 
-        $('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'));
+        jQuery('head').append(jQuery('<link rel="stylesheet" type="text/css" />').attr('href', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'));
 
         // The proctorjsurl is per-user and time-encoded unique, so there is no point in tryng to cache it.
         function loadproctorui() {
+            if (typeof IntegrityAdvocate !== 'undefined') {
+                return;
+            }
             jQuery.getScript(decodeEntities(proctorjsurl))
                     .done(function (script, textStatus) {
                         window.console.log('M.block_integrityadvocate.blockinit::Proctoring JS loaded');
                         jQuery(document).bind('IA_Ready', function (e) {
                             window.console.log('M.block_integrityadvocate.blockinit::IA_Ready event fired');
-                            jQuery('#user-notifications').css({'background-image': 'none'}).height('auto');
+                            elt_usernotifications.css({'background-image': 'none'}).height('auto');
                             switch (true) {
                                 case (is_quiz_attempt):
                                     // On quizzes, disable the submit button and hide the questions until IA is ready.
@@ -55,24 +59,24 @@ M.block_integrityadvocate = {
                                 case (is_scorm_entry):
                                     jQuery('div[role="main"]').find('*').show();
                                     jQuery('#block_integrityadvocate_loading').remove();
-                                    $(window).on('beforeunload', function () {
+                                    jQuery(window).on('beforeunload', function () {
                                         window.console.log('Exiting the window - close the IA session');
                                         window.IntegrityAdvocate.endSession();
                                     });
-                                    $('#scormviewform input[type="submit"]').off('click.block_integrityadvocate').click();
+                                    elt_scorm_enter.removeAttr('disabled').off('click.block_integrityadvocate').click().attr('disabled', 'disabled');
                                     break;
                             }
                         });
                     })
                     .fail(function (jqxhr, settings, exception) {
-                        jQuery('#user-notifications').css({'background-image': 'none'}).height('auto');
+                        elt_usernotifications.css({'background-image': 'none'}).height('auto');
                         var msg = M.str.block_integrityadvocate.proctorjs_load_failed;
                         if (exception.toString() !== 'error') {
                             msg += "Error details:\n" + exception.toString();
                         }
                         window.console.log(arguments);
                         window.console.log(msg);
-                        jQuery('#user-notifications').html('<div class="alert alert-danger alert-block fade in" role="alert" data-aria-autofocus="true">' + msg + '</div>');
+                        elt_usernotifications.html('<div class="alert alert-danger alert-block fade in" role="alert" data-aria-autofocus="true">' + msg + '</div>');
                         jQuery('div[role="main"]').find('*').show();
                         jQuery('#block_integrityadvocate_loading').remove();
                     });
@@ -83,11 +87,18 @@ M.block_integrityadvocate = {
         // Load the proctoring UI except if we are on the SCORM entry page, where we want to trigger it on button click.
         if (is_scorm_entry) {
             window.console.log('This is a SCORM entry page');
-            jQuery('#scormviewform input[type="submit"]').on('click.block_integrityadvocate', function (e) {
+            elt_scorm_enter.on('click.block_integrityadvocate', function (e) {
+                jQuery('#scormviewform input[type="submit"]').attr('disabled', 'disabled');
                 e.preventDefault();
 
                 jQuery('div[role="main"]').find('*').hide();
-                jQuery('#user-notifications').append('<i id="block_integrityadvocate_loading" class="fa fa-spinner fa-spin" style="font-size:72px"></i>');
+                elt_usernotifications.append('<i id="block_integrityadvocate_loading" class="fa fa-spinner fa-spin" style="font-size:72px"></i>');
+                var offset = elt_usernotifications.offset();
+                $('html, body').animate({
+                    scrollTop: offset.top - 50,
+                    scrollLeft: offset.left - 20
+                });
+
                 loadproctorui();
 
                 return false;
