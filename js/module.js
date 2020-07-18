3cf74a52ff01477fdbbbf6119e20019a405c2562
vhmark@gmail.com <vhmark@gmail.com>
2020-07-18 10:21:10 -0700

    Launch IA on SCORM Enter button no on page visit

--- js/module.js
+++ js/module.js
@@ -26,6 +26,7 @@ M.block_integrityadvocate = {
 
         var is_quiz_attempt = (document.body.id === 'page-mod-quiz-attempt');
         var is_scorm_entry = (document.body.id === 'page-mod-scorm-view');
+        window.console.log('is_scorm_entry=', is_scorm_entry);
 
         //@url https://stackoverflow.com/a/31350391
         function decodeEntities(encodedString) {
@@ -34,6 +35,7 @@ M.block_integrityadvocate = {
             return textArea.value;
         }
 
+        $('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'));
 
         // The proctorjsurl is per-user and time-encoded unique, so there is no point in tryng to cache it.
         function loadproctorui() {
@@ -50,13 +52,14 @@ M.block_integrityadvocate = {
                                     jQuery('#block_integrityadvocate_hidequiz').remove();
                                     jQuery('#responseform, #scormpage, div[role="main"]').show()
                                     break;
-                                case (is_scorm_entry) :
-//                                    jQuery('#scormviewform').attr('action', is_scorm_entry);
-//                                    jQuery('#scormviewform').submit();
+                                case (is_scorm_entry):
+                                    jQuery('div[role="main"]').find('*').show();
+                                    jQuery('#block_integrityadvocate_loading').remove();
                                     $(window).on('beforeunload', function () {
                                         window.console.log('Exiting the window - close the IA session');
                                         window.IntegrityAdvocate.endSession();
                                     });
+                                    $('#scormviewform input[type="submit"]').off('click.block_integrityadvocate').click();
                                     break;
                             }
                         });
@@ -70,28 +73,28 @@ M.block_integrityadvocate = {
                         window.console.log(arguments);
                         window.console.log(msg);
                         jQuery('#user-notifications').html('<div class="alert alert-danger alert-block fade in" role="alert" data-aria-autofocus="true">' + msg + '</div>');
+                        jQuery('div[role="main"]').find('*').show();
+                        jQuery('#block_integrityadvocate_loading').remove();
                     });
         }
 
         is_quiz_attempt && jQuery('.mod_quiz-next-nav').attr('disabled', 1);
 
         // Load the proctoring UI except if we are on the SCORM entry page, where we want to trigger it on button click.
-//        if (is_scorm_entry) {
-//            window.console.log('This is a SCORM entry page');
-//            $(window).on('beforeunload', function () {
-//                window.IntegrityAdvocate.endSession();
-//            });
-//            is_scorm_entry = jQuery('#scormviewform').attr('action');
-//            jQuery('#scormviewform').attr('action', 'return false;')
-//            jQuery('#scormviewform').submit(function (e) {
-//                window.console.log('SCORM Enter button clicked');
-//                loadproctorui();
-//                e.preventDefault();
-//                return false;
-//            });
-//        } else {
-        loadproctorui();
-//        }
+        if (is_scorm_entry) {
+            window.console.log('This is a SCORM entry page');
+            jQuery('#scormviewform input[type="submit"]').on('click.block_integrityadvocate', function (e) {
+                e.preventDefault();
+
+                jQuery('div[role="main"]').find('*').hide();
+                jQuery('#user-notifications').append('<i id="block_integrityadvocate_loading" class="fa fa-spinner fa-spin" style="font-size:72px"></i>');
+                loadproctorui();
+
+                return false;
+            });
+        } else {
+            loadproctorui();
+        }
 
     }
 };
