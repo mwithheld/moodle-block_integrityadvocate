1a4119fd169e53013b2c5e321ed5b844ed5eda95
vhmark@gmail.com <vhmark@gmail.com>
2020-08-09 20:02:27 -0700

    Code tidy

--- overview.php
+++ overview.php
@@ -52,13 +52,17 @@ $debug = false;
 // Gather form data.
 $blockinstanceid = \required_param('instanceid', PARAM_INT);
 $courseid = \required_param('courseid', PARAM_INT);
-// If userid is specified, show info only for that user.
-$userid = \optional_param('userid', 0, PARAM_INT); // Which user to show.
+$userid = \optional_param('userid', 0, PARAM_INT);
 $debug && ia_mu::log(__FILE__ . '::Got param $userid=' . $userid);
 
-// These are only used in the course view.
-$groupid = \optional_param('group', 0, PARAM_ALPHANUMEXT); // Group selected.
-$perpage = \optional_param('perpage', DEFAULT_PAGE_SIZE, PARAM_INT); // How many per page.
+$requestedpage = ($userid ? 'overview-user' : 'overview-course');
+
+if ($requestedpage == 'overview-course') {
+    // These variables are only used in the course view.
+    $groupid = \optional_param('group', 0, PARAM_ALPHANUMEXT); // Group selected.
+    $perpage = \optional_param('perpage', DEFAULT_PAGE_SIZE, PARAM_INT); // How many per page.
+}
+
 // Determine course and context.
 $course = \get_course($courseid);
 if (ia_u::is_empty($course)) {
@@ -66,9 +70,16 @@ if (ia_u::is_empty($course)) {
 }
 $coursecontext = \CONTEXT_COURSE::instance($courseid, MUST_EXIST);
 
-// Check user is logged in and capable of accessing the overview.
+// Check user is logged in *to the course*.
 \require_login($course, false);
 \confirm_sesskey();
+
+// Both overview pages require the blockinstance.
+$blockinstance = \block_instance_by_id($blockinstanceid);
+// Sanity check that we got an IA block instance.
+if (ia_u::is_empty($blockinstance) || !($blockinstance instanceof \block_integrityadvocate) || !isset($blockinstance->context)) {
+    throw new \InvalidArgumentException("Blockinstanceid={$blockinstanceid} is not an instance of block_integrityadvocate=" . var_export($blockinstance, true) . '; context=' . var_export($blockinstance->context, true));
+}
 
 // Find the role to display, defaulting to students.
 // To use the default student role, use second param=ia_mu::get_default_course_role($coursecontext).
@@ -77,12 +88,11 @@ $roleid = \optional_param('role', 0, PARAM_INT);
 // Set up page parameters.
 $PAGE->set_course($course);
 $PAGE->requires->css('/blocks/' . INTEGRITYADVOCATE_SHORTNAME . '/css/styles.css');
-
 $baseurl = new \moodle_url('/blocks/' . INTEGRITYADVOCATE_SHORTNAME . '/overview.php',
         array(
     'instanceid' => $blockinstanceid,
     'courseid' => $courseid,
-    // These are only used in the course view.
+    // These are only used in overview-course.
     'group' => $groupid,
     'perpage' => $perpage,
     'sesskey' => sesskey(),
@@ -91,74 +101,59 @@ $baseurl = new \moodle_url('/blocks/' . INTEGRITYADVOCATE_SHORTNAME . '/overview
         ));
 $PAGE->set_url($baseurl);
 $PAGE->set_context($coursecontext);
-$title = \get_string(($userid ? 'overview_user' : 'overview_course'), INTEGRITYADVOCATE_BLOCK_NAME);
+$title = \get_string(str_replace('-', '_', $requestedpage), INTEGRITYADVOCATE_BLOCK_NAME);
 $PAGE->set_title($title);
 $PAGE->set_heading($title);
 $PAGE->navbar->add($title);
 $PAGE->set_pagelayout('report');
-
-// Both overview pages require the blockinstance, so get it here.
-$blockinstance = \block_instance_by_id($blockinstanceid);
-
-if (ia_u::is_empty($blockinstance) || !($blockinstance instanceof \block_integrityadvocate) || !isset($blockinstance->context)) {
-    throw new \InvalidArgumentException("Blockinstanceid={$blockinstanceid} is not an instance of block_integrityadvocate=" . var_export($blockinstance, true) . '; context=' . var_export($blockinstance->context, true));
-}
-
-// Gather capabilities here for later use
-$hascapability_overview = \has_capability('block/integrityadvocate:overview', $blockinstance->context);
-$hascapability_override = \has_capability('block/integrityadvocate:override', $blockinstance->context);
-
-$PAGE->add_body_class(INTEGRITYADVOCATE_BLOCK_NAME . '-' . ($userid ? 'overview-user' : 'overview-course'));
+$PAGE->add_body_class(INTEGRITYADVOCATE_BLOCK_NAME . '-' . $requestedpage);
 $PAGE->requires->string_for_js('filter', 'moodle');
-
 if ($userid) {
-    // This is the overview-user.php page.
+// This is the overview-user.php page.
     $PAGE->requires->css('/blocks/' . INTEGRITYADVOCATE_SHORTNAME . '/css/jquery.dataTables.min.css');
     $PAGE->requires->jquery_plugin('ui-css');
     $PAGE->requires->css('/blocks/' . INTEGRITYADVOCATE_SHORTNAME . '/css/dataTables.fontAwesome.css');
 }
-
 
 // Start page output.
 echo $OUTPUT->header();
 echo $OUTPUT->heading($title, 2);
 echo $OUTPUT->container_start(INTEGRITYADVOCATE_BLOCK_NAME);
 
-// If there is an error, stops further processing, but still displays the page footer.
-$continue = true;
+// Gather capabilities for later use.
+$hascapability_overview = \has_capability('block/integrityadvocate:overview', $blockinstance->context);
+$hascapability_override = \has_capability('block/integrityadvocate:override', $blockinstance->context);
 
-// If the block instance is not configured yet, simply return empty result.
-if ($configerrors = $blockinstance->get_config_errors()) {
-    // No visible IA block found with valid config, so skip any output.
-    if ($hascapability_overview) {
-        echo implode(ia_output::BRNL, $configerrors);
-    }
-    $continue = false;
-}
-$continue && $debug && ia_mu::log(__FILE__ . "::Got \$blockinstance with apikey={$blockinstance->config->apikey}; appid={$blockinstance->config->appid}");
+switch (true) {
+    case ($configerrors = $blockinstance->get_config_errors()):
+        $debug && ia_mu::log(__FILE__ . '::No visible IA block found with valid config; $configerrors=' . ia_u::var_dump($configerrors));
+        // Instructors see the errors on-screen.
+        if ($hascapability_overview) {
+            echo implode(ia_output::BRNL, $configerrors);
+        }
+        break;
 
-// Check site and course completion are set up.
-if ($setuperrors = ia_mu::get_completion_setup_errors($course)) {
-    foreach ($setuperrors as $err) {
-        echo get_string($err, INTEGRITYADVOCATE_BLOCK_NAME) . "<br/>\n";
-    }
-    $continue = false;
-}
+    case($setuperrors = ia_mu::get_completion_setup_errors($course)):
+        $debug && ia_mu::log(__FILE__ . '::Got completion setup errors; $setuperrors=' . ia_u::var_dump($setuperrors));
+        foreach ($setuperrors as $err) {
+            echo get_string($err, INTEGRITYADVOCATE_BLOCK_NAME) . ia_output::BRNL;
+        }
+        break;
 
-if ($continue) {
-    // Check if module have been selected in config.
-    $modules = block_integrityadvocate_get_course_ia_modules($courseid);
-    if (is_string($modules)) {
+    case (is_string($modules = block_integrityadvocate_get_course_ia_modules($courseid))):
+        $debug && ia_mu::log(__FILE__ . '::The course has no IA modules');
+
         echo get_string($modules, INTEGRITYADVOCATE_BLOCK_NAME) . ia_output::BRNL;
-        $continue = false;
-    }
-    $debug && ia_mu::log(basename(__FILE__) . '::Got module count=' . ia_u::count_if_countable($modules));
-}
+        break;
 
-if ($continue) {
-    // Both course and user pages use this JS to kick off interactive features.
-    $PAGE->requires->js_call_amd('block_integrityadvocate/init', 'init');
-    require_once(($userid ? 'overview-user' : 'overview-course') . '.php');
+    default:
+        $debug && ia_mu::log(__FILE__ . "::Got \$blockinstance with apikey={$blockinstance->config->apikey}; appid={$blockinstance->config->appid}");
+
+        // All overview pages use this JS for interactive features.
+        $PAGE->requires->js_call_amd('block_integrityadvocate/init', 'init');
+
+        // Open the requested overview page.
+        require_once($requestedpage . '.php');
 }
 
 echo $OUTPUT->container_end();
