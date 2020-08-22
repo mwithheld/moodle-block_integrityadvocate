e19b89e5a4969498f7d96369a22af8a921a8349e
vhmark@gmail.com <vhmark@gmail.com>
2020-08-21 20:58:09 -0700

    Move feature control into an array

--- lib.php
+++ lib.php
@@ -90,11 +90,11 @@ const INTEGRITYADVOCATE_PRIVACY_EMAIL = 'admin@integrityadvocate.com';
 /** @var string Optionally used for remote debugging */
 const INTEGRITYADVOCATE_LOG_TOKEN = 'fab8d2aa-69a0-4b03-8063-b41b215f2e32';
 
-/** @var bool Feature control: Enable/disable override functionality */
-const INTEGRITYADVOCATE_FEATURE_OVERRIDE = true;
-
-/** @var bool Feature control: Enable/disable showing list of modules in the course block */
-const INTEGRITYADVOCATE_FEATURE_COURSE_MODULELIST = true;
+/** @var [key=bool] Feature control: Enable/disable features easily */
+$block_integrityadvocate_features = [];
+$block_integrityadvocate_features['override'] = true;
+$block_integrityadvocate_features['coursemodulelist'] = true;
+$block_integrityadvocate_features['overviewmodule'] = true;
 
 /** @var string Determines where to send error logs.
  * For values, see MoodleUtility::log()'s switch statement.
