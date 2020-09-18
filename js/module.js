800e1f1cef6c4a74efec887528df109f43118597
vhmark@gmail.com <vhmark@gmail.com>
2020-09-17 17:23:00 -0700

    Fix JS permissions error checking window.opener.IntegrityAdvocate

--- js/module.js
+++ js/module.js
@@ -34,7 +34,6 @@ M.block_integrityadvocate = {
         textArea.innerHTML = encodedString;
         return textArea.value;
     },
-
     /**
      * Stuff to do when the proctor UI is loaded.
      * Hide the loading gif and show the main content.
@@ -85,22 +84,35 @@ M.block_integrityadvocate = {
      * @returns null Nothing.
      */
     loadProctorUi: function(proctorjsurl) {
-        var debug = false;
+        var debug = true;
         var self = M.block_integrityadvocate;
         debug && window.console.log('M.block_integrityadvocate.loadProctorUi::Started with proctorjsurl=', proctorjsurl);
         if (!/^https:\/\/ca\.integrityadvocateserver\.com\/participants\/integrity\?appid=.*/.test(proctorjsurl)) {
             window.console.error('M.block_integrityadvocate.loadProctorUi::Invalid input param');
             return;
         }
-        // To prevent double-loading of the IA logic, check if IA is already loaded in this window or its opener/parent.
-        if (typeof window.IntegrityAdvocate !== 'undefined' ||
-                (typeof window.opener !== 'undefined' && window.opener !== null && typeof window.opener.IntegrityAdvocate !== 'undefined' && window.opener.IntegrityAdvocate !== 'undefined') ||
-                (typeof window.parent !== 'undefined' && window.parent !== null && typeof window.parent.IntegrityAdvocate !== 'undefined' && window.parent.IntegrityAdvocate !== 'undefined')
-                ) {
-            window.console.log('M.block_integrityadvocate.loadProctorUi::IntegrityAdvocate is already loaded');
-            // Hide the loading gif and show the main content.
-            self.proctorUILoaded();
-            return;
+        try {
+            debug && window.console.log('M.block_integrityadvocate.loadProctorUi::About to check for window.IntegrityAdvocate=', window.IntegrityAdvocate);
+            // To prevent double-loading of the IA logic, check if IA is already loaded in this window or its opener|parent.
+            if (typeof window.IntegrityAdvocate !== 'undefined' ||
+                    (typeof window.opener !== 'undefined' && window.opener !== null && Object.keys(window.opener).length && !window.opener.closed && typeof window.opener.IntegrityAdvocate !== 'undefined' && window.opener.IntegrityAdvocate !== 'undefined') ||
+                    (typeof window.parent !== 'undefined' && window.parent !== null && Object.keys(window.parent).length && !window.parent.closed && typeof window.parent.IntegrityAdvocate !== 'undefined' && window.parent.IntegrityAdvocate !== 'undefined')
+                    ) {
+                window.console.log('M.block_integrityadvocate.loadProctorUi::IntegrityAdvocate is already loaded');
+                // Hide the loading gif and show the main content.
+                self.proctorUILoaded();
+                return;
+            }
+        } catch (e) {
+            window.console.log('M.block_integrityadvocate.loadProctorUi::Caught exception e', e);
+            if (e.toString().toLowerCase().indexOf('permission denied') !== -1) {
+                // Ignore the error b/c it means the window.opener|parent.IntegrityAdvocate object does not exist is not accessible.
+                // We do not need to close the loading gif and exit this loadProctorUi() function.
+                window.console.log('M.block_integrityadvocate.loadProctorUi::Ignore the permission denied error and proceed to load the IA script');
+            } else {
+                // Some other problem - let it blow up.
+                throw e;
+            }
         }
         $.getScript(self.decodeEntities(proctorjsurl))
                 .done(function() {
