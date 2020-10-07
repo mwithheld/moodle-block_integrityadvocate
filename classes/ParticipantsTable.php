1f8dc2f536d93a4791dceea87e5cc1d1f2b88e74
vhmark@gmail.com <vhmark@gmail.com>
2020-10-06 21:25:52 -0700

    Fix: wrong get_participant_photo_output() params

--- classes/ParticipantsTable.php
+++ classes/ParticipantsTable.php
@@ -141,114 +141,139 @@ class ParticipantsTable extends \core_user\participants_table {
     public function populate_from_blockinstance(\block_integrityadvocate $blockinstance) {
         $debug = false || Logger::do_log_for_function(__CLASS__ . '::' . __FUNCTION__);
         $fxn = __CLASS__ . '::' . __FUNCTION__;
-        $debug && Logger::log($fxn . '::Started with $blockinstance->instance->id=' . $blockinstance->instance->id);
+        $debugvars = $fxn . "::Started with \$blockinstance->instance->id={$blockinstance->instance->id}";
+        $debug && Logger::log($debugvars);
 
         // Sanity check.
         if (ia_u::is_empty($blockinstance) || !is_numeric($courseid = $blockinstance->get_course()->id)) {
             $msg = 'Input params are invalid';
-            Logger::log($fxn . '::Started with $blockinstance->instance->id=' . $blockinstance->instance->id);
-            Logger::log($fxn . '::' . $msg);
+            Logger::log($debugvars . '::' . $msg);
             throw new \InvalidArgumentException($msg);
         }
 
-        // I am choosing to *not* get participants from the IA API/participants endpoint because...
-        // We only get max 10 results per query in an order that does not match the $this->rawdata list.
-        // And unenrolled users remain in the IA-side participants list, so...
-        // It is slow to get all pages of results.
-        // And I need the session data for each user anyway, which is not included in the GET api/course/{courseid}/participants data.
-        //
-        // Use Guzzle for performance via async parallel GET requests.
-        // Ref https://blog.programster.org/php-async-curl-requests.
-        // Ref http://docs.guzzlephp.org/en/stable/quickstart.html.
-        require_once(dirname(__DIR__) . '/vendor/autoload.php');
-        $requestapiurl = INTEGRITYADVOCATE_BASEURL . INTEGRITYADVOCATE_API_PATH . ia_api::ENDPOINT_PARTICIPANT;
-        $client = new \GuzzleHttp\Client([
-            'base_uri' => $requestapiurl, // Base URI is used with relative requests
-            'timeout' => 30.0, // You can set any number of default request options.
-            'verify' => false, // Disable certificate verification (this is insecure!).
-        ]);
-
-        $requesttimestamp = time();
-        $microtime = explode(' ', microtime());
-        $nonce = $microtime[1] . substr($microtime[0], 2, 6);
-        $requestsignature = ia_api::get_request_signature($requestapiurl, 'GET', $requesttimestamp, $nonce, $blockinstance->config->apikey, $appid = $blockinstance->config->appid);
-        $authheader = 'amx ' . $appid . ':' . $requestsignature . ':' . $nonce . ':' . $requesttimestamp;
-
-        $promises = [];
-
-        // The var $this->rawdata contains all the users for *this page* of the participants table.
-        $debug && Logger::log($fxn . '::We should get data for ' . count($this->rawdata) . ' users');
-        foreach ($this->rawdata as $u) {
-            $params = ['participantidentifier' => $u->id, 'courseid' => $courseid];
-            $debug && Logger::log($fxn . '::About to get data for params=' . ia_u::var_dump($params, true));
-            $promise = $client->getAsync($requestapiurl, [
-                'headers' => ['Authorization' => $authheader],
-                'query' => $params,
+        if (FeatureControl::OVERVIEW_COURSE_USE_GUZZLE) {
+            // I am choosing to *not* get participants from the IA API/participants endpoint because...
+            // We only get max 10 results per query in an order that does not match the $this->rawdata list.
+            // And unenrolled users remain in the IA-side participants list, so...
+            // It is slow to get all pages of results.
+            // And I need the session data for each user anyway, which is not included in the GET api/course/{courseid}/participants data.
+            //
+            // Use Guzzle for performance via async parallel GET requests.
+            // Ref https://blog.programster.org/php-async-curl-requests.
+            // Ref http://docs.guzzlephp.org/en/stable/quickstart.html.
+            require_once(dirname(__DIR__) . '/vendor/autoload.php');
+            $requestapiurl = INTEGRITYADVOCATE_BASEURL . INTEGRITYADVOCATE_API_PATH . ia_api::ENDPOINT_PARTICIPANT;
+            $client = new \GuzzleHttp\Client([
+                'base_uri' => $requestapiurl, // Base URI is used with relative requests
+                'timeout' => 30.0, // You can set any number of default request options.
+                'verify' => false, // Disable certificate verification (this is insecure!).
             ]);
-            $debug && Logger::log($fxn . "::Sent getAsync with requestapiurl={$requestapiurl}; participantidentifier={$u->id}; courseid={$courseid}"); //; url={$url}
-            $promise->then(
-                    /**
-                     * On Guzzle response, parse the participant.
-                     */
-                    function (\Psr\Http\Message\ResponseInterface $response) use ($blockinstance, $debug) {
-                $fxn = __CLASS__ . '::' . __FUNCTION__;
-                $debug && Logger::log($fxn . '::Promise->then started with response=' . ia_u::var_dump($response, true));
-                if (ia_u::is_empty($response) || $response->getStatusCode() !== 200 || ia_u::is_empty($response->getBody())) {
-                    $debug && Logger::log($fxn . '::Invalid response so skipping');
-                    return;
+
+            $requesttimestamp = time();
+            $microtime = explode(' ', microtime());
+            $nonce = $microtime[1] . substr($microtime[0], 2, 6);
+            $requestsignature = ia_api::get_request_signature($requestapiurl, 'GET', $requesttimestamp, $nonce, $blockinstance->config->apikey, $appid = $blockinstance->config->appid);
+            $authheader = 'amx ' . $appid . ':' . $requestsignature . ':' . $nonce . ':' . $requesttimestamp;
+
+            $promises = [];
+
+            // The var $this->rawdata contains all the users for *this page* of the participants table.
+            $debug && Logger::log($fxn . '::We should get data for ' . count($this->rawdata) . ' users');
+            foreach ($this->rawdata as $u) {
+                $params = ['participantidentifier' => $u->id, 'courseid' => $courseid];
+                $debug && Logger::log($fxn . '::About to get data for params=' . ia_u::var_dump($params, true));
+                $promise = $client->getAsync($requestapiurl, [
+                    'headers' => ['Authorization' => $authheader],
+                    'query' => $params,
+                ]);
+                $debug && Logger::log($fxn . "::Sent getAsync with requestapiurl={$requestapiurl}; participantidentifier={$u->id}; courseid={$courseid}"); //; url={$url}
+                $promise->then(
+                        /**
+                         * On Guzzle response, parse the participant.
+                         */
+                        function (\Psr\Http\Message\ResponseInterface $response) use ($blockinstance, $debug) {
+                    $fxn = __CLASS__ . '::' . __FUNCTION__;
+                    $debug && Logger::log($fxn . '::Promise->then started with response=' . ia_u::var_dump($response, true));
+                    if (ia_u::is_empty($response) || $response->getStatusCode() !== 200 || ia_u::is_empty($response->getBody())) {
+                        $debug && Logger::log($fxn . '::Invalid response so skipping');
+                        return;
+                    }
+
+                    $responseparsed = json_decode($response->getBody());
+                    if (ia_u::is_empty($responseparsed) && json_last_error() === JSON_ERROR_NONE) {
+                        throw new \Exception('Failed to json_decode');
+                    }
+                    $debug && Logger::log($fxn . '::After json_decode, got $responseparsed=' . ia_u::var_dump($responseparsed, true));
+
+                    $participant = ia_api::parse_participant($responseparsed);
+                    if (ia_u::is_empty($participant) || !isset($participant->participantidentifier)) {
+                        $debug && Logger::log($fxn . '::Empty participant');
+                        return;
+                    } else {
+                        $debug && Logger::log($fxn . '::Got a participant with id=' . $participant->participantidentifier);
+                    }
+
+                    $this->rawdata[$participant->participantidentifier]->iadata = ia_output::get_participant_summary_output($blockinstance, $participant, false, true, false);
+                    $this->rawdata[$participant->participantidentifier]->iaphoto = ia_output::get_participant_photo_output($participant->participantidentifier, $participant->participantphoto, $participant->status, $participant->email);
+                },
+                        /**
+                         * On Guzzle error, log if non-404.
+                         */
+                        function (\GuzzleHttp\Exception\RequestException $e) use ($debug, $fxn) {
+                    if ($e->getResponse()->getStatusCode() == 404) {
+                        // Do not bother logging except in debug mode.
+                        $debug && Logger::log($fxn . '::Ignoring a 400-level error: $e=' . str_replace(array("\n", "\r"), '', $e->getMessage()));
+                        return;
+                    }
+                    // The getMessage() includes url, response code, and body.
+                    Logger::log($fxn . '::Got a Guzzle exception=' . str_replace(array("\n", "\r"), '', $e->getMessage()));
                 }
+                );
+                $promises[] = $promise;
+            }
 
-                $responseparsed = json_decode($response->getBody());
-                if (ia_u::is_empty($responseparsed) && json_last_error() === JSON_ERROR_NONE) {
-                    throw new \Exception('Failed to json_decode');
-                }
-                $debug && Logger::log($fxn . '::After json_decode, got $responseparsed=' . ia_u::var_dump($responseparsed, true));
-
-                $participant = ia_api::parse_participant($responseparsed);
-                if (ia_u::is_empty($participant) || !isset($participant->participantidentifier)) {
-                    $debug && Logger::log($fxn . '::Empty participant');
-                    return;
-                } else {
-                    $debug && Logger::log($fxn . '::Got a participant with id=' . $participant->participantidentifier);
-                }
-
-                $this->rawdata[$participant->participantidentifier]->iadata = ia_output::get_participant_summary_output($blockinstance, $participant, false, true, false);
-
-                // Participant photo.
-                $this->rawdata[$participant->participantidentifier]->iaphoto = ia_output::get_participant_photo_output($participant);
-            },
-                    /**
-                     * On Guzzle error, log if non-404.
-                     */
-                    function (\GuzzleHttp\Exception\RequestException $e) use ($debug, $fxn) {
-                if ($e->getResponse()->getStatusCode() == 404) {
-                    // Do not bother logging except in debug mode.
+            foreach ($promises as $promise) {
+                try {
+                    $promise->wait();
+                } catch (\GuzzleHttp\Exception\ClientException $e) {
+                    // Ignore 400-level errors. Ref https://stackoverflow.com/a/30957410.
                     $debug && Logger::log($fxn . '::Ignoring a 400-level error: $e=' . str_replace(array("\n", "\r"), '', $e->getMessage()));
-                    return;
+                    continue;
+                } catch (\Exception $e) {
+                    // Catch and log other errors, attempt to continue.
+                    Logger::log($fxn . '::Ignoring an error: $e=' . get_class($e) . '; message=' . str_replace(array("\n", "\r"), '', $e->getMessage()));
+                    continue;
                 }
-                // The getMessage() includes url, response code, and body.
-                Logger::log($fxn . '::Got a Guzzle exception=' . str_replace(array("\n", "\r"), '', $e->getMessage()));
             }
-            );
-            $promises[] = $promise;
+        } else {
+            $debug && Logger::log($fxn . '::Not using Guzzle; use Api::get_participants()');
+            $participants = ia_api::get_participants($blockinstance->config->apikey, $blockinstance->config->appid, $courseid);
+            $debug && Logger::log($fxn . '::Got participants=' . ia_u::var_dump($participants));
+
+            foreach ($this->rawdata as $u) {
+                $debug && Logger::log($fxn . "::Try to match in participant data for \$u->id={$u->id}; \$u->email={$u->email}; \$u->firstname={$u->firstname}; \$u->lastname={$u->lastname}");
+                if (!isset($participants[$u->id])) {
+                    $debug && Logger::log($fxn . '::Skip: No IA data found');
+                    continue;
+                }
+                $participant = $participants[$u->id];
+
+                // Commented out the email check b/c cons outweigh pros:...
+                // Z- Pro: Filters out bad data returned from IA.
+                // Z- Cons: Users cannot change their email address.
+                //if ($participant->email !== $u->email) {
+                //    $debug && Logger::log($fxn . "::Skip: Participant email={$participant->email} does not match \$u->email={$u->email}");
+                //    continue;
+                //}
+                $debug && Logger::log($fxn . "::From userid={$u->id}, found participant=" . ia_u::var_dump($participant));
+
+                // Populate table data: Summary and photo columns.
+                $u->iadata = ia_output::get_participant_summary_output($blockinstance, $participant, false, true, false);
+                $u->iaphoto = ia_output::get_participant_photo_output($participant->participantidentifier, $participant->participantphoto, $participant->status, $participant->email);
+            }
         }
 
-        foreach ($promises as $promise) {
-            try {
-                $promise->wait();
-            } catch (\GuzzleHttp\Exception\ClientException $e) {
-                // Ignore 400-level errors. Ref https://stackoverflow.com/a/30957410.
-                $debug && Logger::log($fxn . '::Ignoring a 400-level error: $e=' . str_replace(array("\n", "\r"), '', $e->getMessage()));
-                continue;
-            } catch (\Exception $e) {
-                // Catch and log other errors, attempt to continue.
-                Logger::log($fxn . '::Ignoring an error: $e=' . get_class($e) . '; message=' . str_replace(array("\n", "\r"), '', $e->getMessage()));
-                continue;
-            }
-        }
-
-        // Disabled on purpose: $debug && Logger::log($fxn . "::About to return; \$this->rawdata=" . ia_u::var_dump($this->rawdata, true));.
-        $debug && Logger::log($fxn . '::About to return');
+        $debug && Logger::log($fxn . '::About to return count($this->rawdata)=' . ia_u::count_if_countable($this->rawdata));
     }
 
     /**
