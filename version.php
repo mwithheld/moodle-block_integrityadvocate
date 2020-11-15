93dccaebed3d51d0c6ee26bc5c0f891561c5dbe2
mwithheld <796986+mwithheld@users.noreply.github.com>
2020-11-15 11:17:37 -0800

    Bump version to 2020100103

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
  */
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2020100102;
-$plugin->release = 2020100102;
+$plugin->version = 2020100103;
+$plugin->release = 2020100103;
 $plugin->requires = 2018051700; // Moodle 3.5.0.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'block_integrityadvocate';
