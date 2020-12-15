cdb991f218ca9593e8234588406eba12c1bfb9c1
mwithheld <796986+mwithheld@users.noreply.github.com>
2020-12-15 11:13:17 -0800

    Add missing use statement

--- overview-user.php
+++ overview-user.php
@@ -27,6 +27,7 @@ namespace block_integrityadvocate;
 use block_integrityadvocate\Api as ia_api;
 use block_integrityadvocate\Logger as Logger;
 use block_integrityadvocate\MoodleUtility as ia_mu;
+use block_integrityadvocate\Output as ia_output;
 use block_integrityadvocate\Status as ia_status;
 use block_integrityadvocate\Utility as ia_u;
 
