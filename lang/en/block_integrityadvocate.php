8c7243de1a02a7c04ecebad61e8b58e07313461b
mwithheld <796986+mwithheld@users.noreply.github.com>
2021-02-03 10:45:22 -0800

    Remove redundant Troubleshooting link

--- lang/en/block_integrityadvocate.php
+++ lang/en/block_integrityadvocate.php
@@ -23,7 +23,7 @@
  */
 $string['pluginname'] = 'Integrity Advocate';
 $string['proctorjs_load_failed'] = 'The Integrity Advocate proctoring module failed to load - contact your instructor for assistance.';
-$string['studentmessage'] = 'This page uses the Integrity Advocate proctoring service.<br /><a href="https://www.integrityadvocate.com/privacy-policy-for-end-users" target="_blank">Privacy</a><br /><a href="https://support.integrityadvocate.com/" target="_blank">Troubleshooting</a><br /><a href="https://support.integrityadvocate.com/" target="_blank">Support</a>';
+$string['studentmessage'] = 'This page uses the Integrity Advocate proctoring service.<br /><a href="https://www.integrityadvocate.com/privacy-policy-for-end-users" target="_blank">Privacy</a><br /><a href="https://support.integrityadvocate.com/" target="_blank">Support</a>';
 
 $string['cachedef_perrequest'] = 'Remember values during a single request';
 $string['cachedef_persession'] = 'Remember values during the user session';
