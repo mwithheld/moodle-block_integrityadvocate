ab3cc8ffd361e730612448c876ebfd4e87c4225d
mwithheld <796986+mwithheld@users.noreply.github.com>
2024-04-14 07:47:57 -0700

    Change isHttpUrl to simpler regex
    Github security was unhappy

--- js/module.js
+++ js/module.js
@@ -278,8 +278,7 @@ M.block_integrityadvocate = {
     },
     isHttpUrl: function(str) {
         // Source: Adapted from https://thispointer.com/javascript-check-if-string-is-url/ .
-        // Disabled for now: return /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/.test(str); .
-        // Source: https://mathiasbynens.be/demo/url-regex > @diegoperini.
-        return /%^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\x{00a1}-\x{ffff}][a-z0-9\x{00a1}-\x{ffff}_-]{0,62})?[a-z0-9\x{00a1}-\x{ffff}]\.)+(?:[a-z\x{00a1}-\x{ffff}]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$%iuS/.test(str);
+        // This is not meant to be the perfect regex, just a quick sanity check.
+        return /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/.test(str);
       }
 };
