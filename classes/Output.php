14c5c6426421f618bf054983d7080efaf39f2598
mwithheld <796986+mwithheld@users.noreply.github.com>
2024-07-20 21:03:14 -0700

    Code formatting

--- classes/Output.php
+++ classes/Output.php
@@ -114,8 +114,8 @@ class Output {
             'M.block_integrityadvocate.blockinit',
             [
                 $proctorjsurl,
-                (int)(isset($blockinstance->config->proctorquizinfopage)?$blockinstance->config->proctorquizinfopage:0),
-                (int)(isset($blockinstance->config->proctorquizreviewpages)?$blockinstance->config->proctorquizreviewpages:0),
+                (int)(isset($blockinstance->config->proctorquizinfopage) ? $blockinstance->config->proctorquizinfopage : 0),
+                (int)(isset($blockinstance->config->proctorquizreviewpages) ? $blockinstance->config->proctorquizreviewpages : 0),
                 (int)$quizshowsreviewpage,
             ],
             false,
