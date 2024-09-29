e06d6c3d091f77bfd359c266f1105498309d604c
mwithheld <796986+mwithheld@users.noreply.github.com>
2024-09-28 18:34:35 -0700

    Code checker fixes

--- tests/behat/behat_block_integrityadvocate.php
+++ tests/behat/behat_block_integrityadvocate.php
@@ -1,8 +1,33 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * IntegrityAdvocate behat helpers.
+ *
+ * @package    block_integrityadvocate
+ * @copyright  IntegrityAdvocate.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\TableNode;
 
+/**
+ * Behat helper functions.
+ */
 class behat_block_integrityadvocate extends behat_base {
 
     /** @var string String to put at the end of behat debug output */
@@ -11,7 +36,7 @@ class behat_block_integrityadvocate extends behat_base {
     /**
      * Helper to fill fields in the form.
      */
-    private function fillField($fieldname, $value) {
+    private function fill_field($fieldname, $value) {
         $session = $this->getSession();
         $page = $session->getPage();
         $field = $page->findField($fieldname);
@@ -46,7 +71,7 @@ class behat_block_integrityadvocate extends behat_base {
 
             if (isset($CFG->{$cfgname})) {
                 $fieldvalue = $CFG->{$cfgname};
-                $this->fillField($fieldname, $fieldvalue);
+                $this->fill_field($fieldname, $fieldvalue);
             } else {
                 throw new Exception("The \$CFG->{$cfgname} is not set.");
             }
